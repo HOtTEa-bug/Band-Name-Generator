644c7a18ee3a132b40bb44e9ee9a68075684a8bc
Shaini Dasgupta <hottea.code@gmail.com>
2021-08-23 00:29:51 +0530

    first commit

---
+++ BandNameGenerator.py
@@ -0,0 +1,10 @@
+'''[Project-1]
+Topic: Band-Name-Generator. '''
+
+print("---||Welcome to the Band Name Generator||---\n\n")
+
+city = input("# What's the name of your favourite Ice-cream flavour?\n")
+
+pet = input("# What's the name of your favourite animal?\n")
+
+print("Your band name could be " + city + " " + pet)
